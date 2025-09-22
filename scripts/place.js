1dac1794e0dce71f713d6aa7ed117b8f6ab0498f
Eghosa Ordia <eghodia1004@yahoo.com>
2025-09-22 20:58:15 +0100

    completed assignment

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,16 @@
 const lastModified = document.querySelector("#lastmodified");
 
 lastModified.textContent = document.lastModified;
+const calculateWindChill = (temperature, windspeed) => {
+  if (temperature > 10 || windspeed <= 4.8) {
+    return "N/A";
+  }
+
+  let windchill =
+    13.12 +
+    0.6215 * temperature -
+    11.37 * Math.pow(windspeed, 0.16) +
+    0.3965 * temperature * Math.pow(windspeed, 0.16);
+
+  return windchill;
+};
