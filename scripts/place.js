46084028e3b88f84fceaeade522d954909cb63d9
Eghosa Ordia <eghodia1004@yahoo.com>
2025-09-22 21:12:04 +0100

    feat: remove trailing slashes and added windchill function

--- scripts/place.js
+++ scripts/place.js
@@ -1,4 +1,5 @@
 const lastModified = document.querySelector("#lastmodified");
+const windChillSpan = document.querySelector("#windChill");
 
 lastModified.textContent = document.lastModified;
 const calculateWindChill = (temperature, windspeed) => {
@@ -12,5 +13,9 @@ const calculateWindChill = (temperature, windspeed) => {
     11.37 * Math.pow(windspeed, 0.16) +
     0.3965 * temperature * Math.pow(windspeed, 0.16);
 
-  return windchill;
+  return String(Math.round(windchill * 100) / 100);
 };
+let temperature = 5;
+let windspeed = 10;
+const windchill = calculateWindChill(temperature, windspeed);
+windChillSpan.textContent = windchill + "°C";
